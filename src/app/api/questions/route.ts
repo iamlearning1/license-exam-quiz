2a2f544ebb7879b6cc17966073d261ca44753a1e
iamlearning1 <d1612k@gmail.com>
2025-07-04 00:18:10 +0200

    change no of questions using env

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -1,10 +1,13 @@
-import { questions } from "@/app/constants";
-import { NextResponse } from "next/server";
+import { questions } from '@/app/constants';
+import { NextResponse } from 'next/server';
 
 export async function GET() {
-  // select 30 random questions from the questions array
+  // select random questions from the questions array
   const shuffledQuestions = questions.sort(() => 0.5 - Math.random());
-  const selectedQuestions = shuffledQuestions.slice(0, 5);
+  const selectedQuestions = shuffledQuestions.slice(
+    0,
+    parseInt(process.env.NO_OF_QUESTIONS || '10')
+  );
 
   return NextResponse.json(selectedQuestions);
 }
